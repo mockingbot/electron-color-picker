8c68596112a6870f9ee323a76ca7cc9dde610b1d
dr-js <dr@dr.run>
2019-07-10 16:18:35 +0800

    electron-color-picker@0.1.3-dev.1
    notable change:
    - fix executable file permission for `source/linux/linux-scrot/scrot`
    - add script to prevent `.tgz` packing on win32 (result permission loss)
    - package update

--- script/generateSpec.js
+++ script/generateSpec.js
@@ -1,11 +1,9 @@
 import { resolve } from 'path'
 import { writeFileSync } from 'fs'
 
-import { indentLine } from 'dr-js/module/common/string'
-
 import { collectSourceRouteMap } from 'dr-dev/module/node/export/parse'
 import { generateExportInfo } from 'dr-dev/module/node/export/generate'
-import { autoAppendMarkdownHeaderLink, renderMarkdownExportPath, renderMarkdownFileLink } from 'dr-dev/module/node/export/renderMarkdown'
+import { getMarkdownFileLink, renderMarkdownAutoAppendHeaderLink, renderMarkdownBlockQuote, renderMarkdownExportPath } from 'dr-dev/module/node/export/renderMarkdown'
 import { runMain } from 'dr-dev/module/main'
 
 import { engines, peerDependencies } from '../package.json'
@@ -13,34 +11,26 @@ import { engines, peerDependencies } from '../package.json'
 const PATH_ROOT = resolve(__dirname, '..')
 const fromRoot = (...args) => resolve(PATH_ROOT, ...args)
 
-const renderMarkdownPackage = () => [
-  renderMarkdownFileLink('package.json'),
-  '> ```',
-  indentLine(JSON.stringify({ engines, peerDependencies }, undefined, 2), '> '),
-  '> ```'
-]
-
 runMain(async (logger) => {
-  logger.log(`collect sourceRouteMap`)
+  logger.log(`generate exportInfoMap`)
   const sourceRouteMap = await collectSourceRouteMap({
     pathRootList: [ fromRoot('source') ],
     pathInfoFilter: ({ name }) => name !== 'index.example.js',
     logger
   })
-
-  logger.log(`generate exportInfo`)
   const exportInfoMap = generateExportInfo({ sourceRouteMap })
 
   logger.log(`output: SPEC.md`)
   writeFileSync(fromRoot('SPEC.md'), [
     '# Specification',
     '',
-    ...autoAppendMarkdownHeaderLink(
+    ...renderMarkdownAutoAppendHeaderLink(
       '#### Export Path',
       ...renderMarkdownExportPath({ exportInfoMap, rootPath: PATH_ROOT }),
       '',
       '#### Package',
-      ...renderMarkdownPackage()
+      getMarkdownFileLink('package.json'),
+      ...renderMarkdownBlockQuote(JSON.stringify({ engines, peerDependencies }, null, 2))
     ),
     ''
   ].join('\n'))
--- script/index.js
+++ script/index.js
@@ -1,8 +1,6 @@
 import { resolve } from 'path'
 import { execSync } from 'child_process'
 
-import { binary } from 'dr-js/module/common/format'
-
 import { getScriptFileListFromPathList } from 'dr-dev/module/node/fileList'
 import { runMain, argvFlag } from 'dr-dev/module/main'
 import { initOutput, packOutput, publishOutput } from 'dr-dev/module/output'
@@ -24,6 +22,7 @@ runMain(async (logger) => {
   execSync(`npm run script-generate-spec`, execOptionRoot)
 
   if (!argvFlag('pack')) return
+  if (process.platform === 'win32') throw new Error('use a *nix platform to pack `.tgz` to preserve correct file permission!')
 
   padLog(`build output`)
   execSync('npm run build-library', execOptionRoot)
@@ -31,10 +30,8 @@ runMain(async (logger) => {
   const fileList = await getScriptFileListFromPathList([ 'library' ], fromOutput)
 
   log(`process output`)
-  let sizeReduce = 0
-  sizeReduce += await minifyFileListWithTerser({ fileList, option: getTerserOption(), rootPath: PATH_OUTPUT, logger })
-  sizeReduce += await processFileList({ fileList, processor: fileProcessorBabel, rootPath: PATH_OUTPUT, logger })
-  log(`size reduce: ${binary(sizeReduce)}B`)
+  await minifyFileListWithTerser({ fileList, option: getTerserOption(), rootPath: PATH_OUTPUT, logger })
+  await processFileList({ fileList, processor: fileProcessorBabel, rootPath: PATH_OUTPUT, logger })
 
   const pathPackagePack = await packOutput({ fromRoot, fromOutput, logger })
   await publishOutput({ flagList: process.argv, packageJSON, pathPackagePack, extraArgs: [ '--userconfig', '~/mockingbot.npmrc' ], logger })
